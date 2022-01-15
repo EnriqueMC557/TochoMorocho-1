bc1d1c8dc242d9415a0bec1aa53a001f7e3aeb1b
EnriqueMC <enrique.mena@rookmotion.com>
2022-01-15 16:33:07 -0600

    Usando git desde pycharm

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def gerry():
 	print('Hola Mundo')
 	return	
@@ -17,8 +20,13 @@ def kike():
 	print("-1 + 1 = 2")
 
 
+def printnp_array(array):
+	print(np.array(array))
+
+
 if __name__ == "__main__":
 	gerry()
 	shei()
 	mata()
 	kike()
+	printnp_array([1, 2, 3, 4])
